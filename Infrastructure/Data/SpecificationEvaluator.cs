52078c726455434b6a6e77f9800328dae786402b
Alejandro <charlie14_1939@hotmail.com>
2020-09-11 01:32:27 -0300

    Lesson 38 Implementing repo with spec

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -7,13 +7,13 @@ namespace Infrastructure.Data
 {
     public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
     {
-        public static IQueryable<TEntity> GetQueryable(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec)
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec)
         {
             var query = inputQuery;
 
             if (spec.Criteria != null)
             {
-                query = query.Where(spec.Criteria);
+                query = query.Where(spec.Criteria); // p => p.ProductTypeId == id
             }
 
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
