6dc2539d33ab4f257b0149b9eb87e534fa362640
Alejandro <charlie14_1939@hotmail.com>
2020-09-18 14:38:32 -0300

    Lesson 61 Working around Sqlite decimal limitation

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -6,10 +6,29 @@ namespace Core.Specifications
 {
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductsWithTypesAndBrandsSpecification()
+        public ProductsWithTypesAndBrandsSpecification(string sort)
         {
             AddIncludes(x => x.ProductBrand);
             AddIncludes(x => x.ProductType);
+            AddOrderBy(x => x.Name);
+
+            if (!string.IsNullOrEmpty(sort))
+            {
+                switch (sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(p => p.Price);
+                        break;
+                    
+                    case "priceDesc":
+                        AddOrderByDescending(p => p.Price);
+                        break;
+                    
+                    default:
+                        AddOrderBy(p => p.Name);   
+                        break;                 
+                }
+            }
         }
 
         public ProductsWithTypesAndBrandsSpecification(int productId) : base(x => x.Id == productId)
