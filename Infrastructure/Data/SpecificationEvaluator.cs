5bc61603e0c5d3acda578311e9355a9012eebf0c
Alejandro <charlie14_1939@hotmail.com>
2020-09-21 19:39:58 -0300

    Lesson 63 Paging part 1

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -12,19 +12,16 @@ namespace Infrastructure.Data
             var query = inputQuery;
 
             if (spec.Criteria != null)
-            {
                 query = query.Where(spec.Criteria); // p => p.ProductTypeId == id
-            }
 
             if (spec.OrderBy != null)
-            {
                 query = query.OrderBy(spec.OrderBy);
-            }
 
             if (spec.OrderByDescending != null)
-            {
                 query = query.OrderBy(spec.OrderByDescending);
-            }
+
+            if (spec.IsPagingEnabled)
+                query = query.Skip(spec.Skip).Take(spec.Take);
 
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
 
