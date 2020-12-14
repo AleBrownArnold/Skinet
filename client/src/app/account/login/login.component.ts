28c0d56b38f24681dbe5b9cc3e417c23d7e1286a
Alejandro <charlie14_1939@hotmail.com>
2020-12-14 18:19:40 -0300

    Lesson 191 Reactive Form Login Component

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+              private accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.createLoginForm();
+  }
+
+  createLoginForm() {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    this.accountService.login(this.loginForm.value).subscribe(() => {
+      console.log('User logged in');
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
