cf5801711c24088b7b1c88900ae4049c724b1d9e
Alejandro <charlie14_1939@hotmail.com>
2021-01-22 22:12:54 -0300

    Lesson 219 Implemented gen rep methods

--- Infrastructure/Services/OrderService.cs
+++ Infrastructure/Services/OrderService.cs
@@ -9,57 +9,59 @@ namespace Infrastructure.Services
 {
     public class OrderService : IOrderService
     {
-        private readonly IGenericRepository<Order> _orderRepo;
-        private readonly IGenericRepository<DeliveryMethod> _dmRepo;
-        private readonly IGenericRepository<Product> _productRepo;
         private readonly IBasketRepository _basketRepo;
-        public OrderService(IGenericRepository<Order> orderRepo, IGenericRepository<DeliveryMethod> dmRepo, IGenericRepository<Product> productRepo,
-        IBasketRepository basketRepo)
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderService(IUnitOfWork unitOfWork, IBasketRepository basketRepo)
         {
+            _unitOfWork = unitOfWork;
             _basketRepo = basketRepo;
-            _productRepo = productRepo;
-            _dmRepo = dmRepo;
-            _orderRepo = orderRepo;
+
         }
 
-    public async Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethodId, string basketId, Address shippingAddress)
-    {
-        var basket = await _basketRepo.GetBasketAsync(basketId);
-
-        var items = new List<OrderItem>();
-        foreach (var item in basket.Items)
+        public async Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethodId, string basketId, Address shippingAddress)
         {
-            var productItem = await _productRepo.GetByIdAsync(item.Id);
-            var itemOrdered = new ProductItemOrdered(productItem.Id, productItem.Name, productItem.PictureUrl);
-            var orderItem =  new OrderItem(itemOrdered, productItem.Price, item.Quantity);
+            var basket = await _basketRepo.GetBasketAsync(basketId);
 
-            items.Add(orderItem);
+            var items = new List<OrderItem>();
+            foreach (var item in basket.Items)
+            {
+                var productItem = await _unitOfWork.Repository<Product>().GetByIdAsync(item.Id);
+                var itemOrdered = new ProductItemOrdered(productItem.Id, productItem.Name, productItem.PictureUrl);
+                var orderItem = new OrderItem(itemOrdered, productItem.Price, item.Quantity);
+
+                items.Add(orderItem);
+            }
+
+            var deliveryMethod = await _unitOfWork.Repository<DeliveryMethod>().GetByIdAsync(deliveryMethodId);
+
+            var subTotal = items.Sum(item => item.Price * item.Quantity);
+
+            var order = new Order(items, buyerEmail, shippingAddress, deliveryMethod, subTotal);
+
+            _unitOfWork.Repository<Order>().Add(order);
+
+            var results = await _unitOfWork.Complete();
+
+            if (results <= 0) return null;
+
+            await _basketRepo.DeleteBasketAsync(basketId);
+
+            return order;
         }
 
-        var deliveryMethod = await _dmRepo.GetByIdAsync(deliveryMethodId);
+        public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
+        {
+            throw new System.NotImplementedException();
+        }
 
-        var subTotal = items.Sum(item => item.Price * item.Quantity);
+        public Task<Order> GetOrderByIdAsync(int id, string buyerEmail)
+        {
+            throw new System.NotImplementedException();
+        }
 
-        var order = new Order(items, buyerEmail, shippingAddress, deliveryMethod, subTotal);
-
-        // TODO: save to db
-
-        return order;
+        public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
+        {
+            throw new System.NotImplementedException();
+        }
     }
-
-    public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public Task<Order> GetOrderByIdAsync(int id, string buyerEmail)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
-    {
-        throw new System.NotImplementedException();
-    }
-}
 }
