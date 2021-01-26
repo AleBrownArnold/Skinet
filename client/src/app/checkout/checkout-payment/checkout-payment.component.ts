13aa5529f87ff7c90411b8227cacb569ee9b773c
Alejandro <charlie14_1939@hotmail.com>
2021-01-26 18:28:36 -0300

    Lesson 248 / Section 19 Added checkout success template

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { NavigationExtras, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { BasketService } from 'src/app/basket/basket.service';
 import { IBasket } from 'src/app/shared/models/basket';
@@ -16,6 +17,7 @@ export class CheckoutPaymentComponent implements OnInit {
 
   constructor(private basketService: BasketService,
               private checkoutService: CheckoutService,
+              private router: Router,
               private toastr: ToastrService) { }
 
   ngOnInit(): void {
@@ -28,6 +30,8 @@ export class CheckoutPaymentComponent implements OnInit {
     this.checkoutService.createOrder(orderToCreate).subscribe((order: IOrder) => {
       this.toastr.success('Order created succesfully');
       this.basketService.deleteLocalBasket(basket.id);
+      const navigationExtras: NavigationExtras = {state: order};
+      this.router.navigate(['checkout/success'], navigationExtras);
       console.log(order);
     }, error => {
       this.toastr.error(error.message);
